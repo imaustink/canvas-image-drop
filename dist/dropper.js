d27696b8dd4515ebd40257f2e4bfe8d46963e869
imaustink <austin@austinkurpuis.com>
2016-11-15 11:43:22 -0800

    Cleanup and performance

--- dist/dropper.js
+++ dist/dropper.js
@@ -11,22 +11,26 @@ module.exports.imagePainter = function imagePainter(settings){
     var canvas = settings.canvas || document.getElementById(settings.id);
     // Get 2D context
     var context = canvas.getContext('2d');
-    // Create an image element for loading our image into
-    var image = document.createElement('img');
     // defauly canvas deminsions
-    var dHeight = canvas.height;
-    var dWidth = canvas.width;
-    
-    // Listen for image loading
-    image.addEventListener('load', function() {
-        // Starting x and y pos
-        var x = 0;
-        var y = 0;
-        
+    var sx = 0;
+    var sy = 0;
+    var sWidth;
+    var sHeight;
+    var dx = 0;
+    var dy = 0;
+    var dWidth;
+    var dHeight;
+
+    // Paint the image
+    function paint(image) {
+
+        sWidth = dWidth = image.width;
+        sHeight = dHeight = image.height;
+
         // Set center of image placement
         function setCenter(xpos, ypos) {
-            x += (xpos - (image.width / 2));
-            y += (ypos - (image.height / 2));
+            dx += (xpos - (dWidth / 2));
+            dy += (ypos - (dHeight / 2));
         }
         
         // Resize image to fit canvas
@@ -35,33 +39,22 @@ module.exports.imagePainter = function imagePainter(settings){
             var hd = image.height - canvas.height;
             
             if(hd > 0 || wd > 0){
-                var rp;
-                if(wd >= hd) rp = wd / (image.width / 100);
-                else rp =  hd / (image.height / 100);
+                var p;
+                if(wd >= hd) p = wd / (image.width / 100);
+                else p =  hd / (image.height / 100);
                 
-                var w = (image.width / 100) * (100 - rp);
-                var h = (image.height / 100) * (100 - rp);
+                var w = (image.width / 100) * (100 - p);
+                var h = (image.height / 100) * (100 - p);
                 
-                var tempCanvas = document.createElement('canvas');
-                var temContext = tempCanvas.getContext('2d');
-                
-                tempCanvas.height = image.height;
-                tempCanvas.width = image.width;
-                
-                temContext.drawImage(image, 0, 0);
-                resizeImage(tempCanvas, w, h, true);
-                
-                image.src = tempCanvas.toDataURL();
-
-                return;
-            }
-            
+                // TODO resize
+                dWidth = w;
+                dHeight = h;
+            }    
         }
         
         // Resize canvas to match image
         if(settings.resize){
-            resizeImage(canvas, dWidth, dHeight, true);
-            
+            // TODO resize
             canvas.width = image.width;
             canvas.height = image.height;
         }
@@ -79,109 +72,24 @@ module.exports.imagePainter = function imagePainter(settings){
     
         // Add custom offset
         if(settings.offset){
-            x += settings.offset.x || 0;
-            y += settings.offset.y || 0;
+            sx += settings.offset.x || 0;
+            sy += settings.offset.y || 0;
         }
         
         // Wipe our canvas
         if(!settings.overlay) clearCanvas();
-        
+
         // Draw image
-        context.drawImage(image, x, y);
+        context.drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
         
-    }, false);
+    }
     
     // Clear canvas helper
     function clearCanvas() {
         context.clearRect(0, 0, canvas.width, canvas.height);
     }
     
-    /**
-     * Hermite resize - fast image resize/resample using Hermite filter. 1 cpu version!
-     * 
-     * @param {HtmlElement} canvas
-     * @param {int} width
-     * @param {int} height
-     * @param {boolean} resize_canvas if true, canvas will be resized. Optional.
-     */
-    function resizeImage(canvas, width, height, resize_canvas) {
-        var width_source = canvas.width;
-        var height_source = canvas.height;
-        width = Math.round(width);
-        height = Math.round(height);
-    
-        var ratio_w = width_source / width;
-        var ratio_h = height_source / height;
-        var ratio_w_half = Math.ceil(ratio_w / 2);
-        var ratio_h_half = Math.ceil(ratio_h / 2);
-    
-        var ctx = canvas.getContext("2d");
-        var img = ctx.getImageData(0, 0, width_source, height_source);
-        var img2 = ctx.createImageData(width, height);
-        var data = img.data;
-        var data2 = img2.data;
-    
-        for (var j = 0; j < height; j++) {
-            for (var i = 0; i < width; i++) {
-                var x2 = (i + j * width) * 4;
-                var weight = 0;
-                var weights = 0;
-                var weights_alpha = 0;
-                var gx_r = 0;
-                var gx_g = 0;
-                var gx_b = 0;
-                var gx_a = 0;
-                var center_y = (j + 0.5) * ratio_h;
-                var yy_start = Math.floor(j * ratio_h);
-                var yy_stop = Math.ceil((j + 1) * ratio_h);
-                for (var yy = yy_start; yy < yy_stop; yy++) {
-                    var dy = Math.abs(center_y - (yy + 0.5)) / ratio_h_half;
-                    var center_x = (i + 0.5) * ratio_w;
-                    var w0 = dy * dy; //pre-calc part of w
-                    var xx_start = Math.floor(i * ratio_w);
-                    var xx_stop = Math.ceil((i + 1) * ratio_w);
-                    for (var xx = xx_start; xx < xx_stop; xx++) {
-                        var dx = Math.abs(center_x - (xx + 0.5)) / ratio_w_half;
-                        var w = Math.sqrt(w0 + dx * dx);
-                        if (w >= 1) {
-                            //pixel too far
-                            continue;
-                        }
-                        //hermite filter
-                        weight = 2 * w * w * w - 3 * w * w + 1;
-                        var pos_x = 4 * (xx + yy * width_source);
-                        //alpha
-                        gx_a += weight * data[pos_x + 3];
-                        weights_alpha += weight;
-                        //colors
-                        if (data[pos_x + 3] < 255)
-                            weight = weight * data[pos_x + 3] / 250;
-                        gx_r += weight * data[pos_x];
-                        gx_g += weight * data[pos_x + 1];
-                        gx_b += weight * data[pos_x + 2];
-                        weights += weight;
-                    }
-                }
-                data2[x2] = gx_r / weights;
-                data2[x2 + 1] = gx_g / weights;
-                data2[x2 + 2] = gx_b / weights;
-                data2[x2 + 3] = gx_a / weights_alpha;
-            }
-        }
-        //clear and resize canvas
-        if (resize_canvas === true) {
-            canvas.width = width;
-            canvas.height = height;
-        } else {
-            ctx.clearRect(0, 0, width_source, height_source);
-        }
-    
-        //draw
-        ctx.putImageData(img2, 0, 0);
-    }
-    
-    // Load image
-    image.src = settings.image.src;
+    paint(settings.image);
     
     return this;
 };
